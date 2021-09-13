a381009dd0ee9699aefcf7e210a985d5c2f543ea
Aldy Tosaleko <generalprjkt@gmail.com>
2021-09-13 14:23:16 +0700

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/conquer_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    conquer_ginkgo-user \
+    conquer_ginkgo-userdebug \
+    conquer_ginkgo-eng
